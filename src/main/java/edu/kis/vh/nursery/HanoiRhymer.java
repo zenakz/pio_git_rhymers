ba586cb42deb30f023e68cdce989ede9cc8da002
Zenakz <Zenakz@github.com>
2020-01-11 17:45:37 +0100

    1.3 2

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,14 +4,15 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
 
 int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
-
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+public int reportRejected() {
+	return totalRejected;
 }
+
+public void countIn(int in) {
+if (!callCheck() && in > peekaboo())
+		totalRejected++;
+		else
+			super.countIn(in);
+}
+}
+//alt oraz strzałki przełączają między otwartymi opknami w środowisku
